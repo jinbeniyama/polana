0f001fe29069a9d04d673976a85e5abc7fb36398
Jin Beniyama <beniyama@ioa.s.u-tokyo.ac.jp>
2023-01-30 02:05:20 +0900

    Clean the script

--- src/pol_WFGS2.py
+++ src/pol_WFGS2.py
@@ -7,21 +7,18 @@ x y fits
 468 1462 wfgs2_221220_0001.2010XC15.Rc.e.cr.fits
 .
 
-Ordainary and Extra-ordinary can be distinguished from fits file.
+Ordainary and Extra-ordinary can be distinguished with fits file.
 """
 import os
-import sys
 import numpy as np
 import pandas as pd
 from argparse import ArgumentParser as ap
 from scipy.spatial import KDTree
-from datetime import datetime
 import sep
 import astropy.io.fits as fits
 
-from dipolana.util import *
-from myplot import mycolor, myls
-
+from polana.util import *
+from polana.visualization import mycolor, myls
 
 
 if __name__ == "__main__":
@@ -31,17 +28,20 @@ if __name__ == "__main__":
         "obj", type=str, 
         help="Object name")
     parser.add_argument(
-        "loc", type=str, 
+        "inp", type=str, nargs="*",
+        help="Input file with appropreate format")
+    parser.add_argument(
+        "--loc", type=str, default="371", 
         help="Observation location (MPC code)")
     parser.add_argument(
-        "inp", type=str, nargs="*",
-        help="Input file with certain format")
+        "--mp", action='store_true',
+        help='Save phase angle in the output for minor planet')
     parser.add_argument(
         "--fitsdir", type=str, default=".",
-        help="Fits directory")
+        help="Directory in which fits exists")
     parser.add_argument(
         "--radius", type=float, default=40, 
-        help="aperture radius in pixel")
+        help="aperture radius in pixel (p_scale = 0.198 arcsec/s)")
     parser.add_argument(
         "--ann_gap", type=float, default=2, 
         help="gap between annulus and circle ")
@@ -58,9 +58,6 @@ if __name__ == "__main__":
         "--out", type=str, default=None,
         help="Output filename")
     parser.add_argument(
-        "--wobgsub", action='store_false', default=True,
-        help='Do not subtract background')
-    parser.add_argument(
         "-p", "--photmap", action='store_true',
         help='create photometry region map (a bit slow)')
     parser.add_argument(
@@ -71,7 +68,7 @@ if __name__ == "__main__":
     outdir = args.outdir
     os.makedirs(outdir, exist_ok=True)
 
-    ## For photometry region png in output directory
+    ## Output photometry region png in the directory
     if args.photmap:
         photmapdir = os.path.join(outdir, "photregion")
         os.makedirs(photmapdir, exist_ok=True)
@@ -79,7 +76,10 @@ if __name__ == "__main__":
     fitsdir = args.fitsdir
     radius = args.radius
     band = args.band
-    print(f"  Aperture radius {radius} pix")
+    # See SCALE in the fits header
+    p_scale = 0.198
+    radius_arcsec = radius * p_scale
+    print(f"  Aperture radius {radius} pix = {radius_arcsec} arcsec")
     print(f"  filter {band}-band")
 
     key_texp = "EXPTIME"
@@ -87,24 +87,25 @@ if __name__ == "__main__":
     key_date = "DATE-OBS"
     # Central UTC
     key_ut = "UT-CEN"
-    # 0, 45, 22.5, 67.5
+    # Angle of retarder 0, 45, 22.5, 67.5
     key_ang = "HWP-AGL"
     # Inverse gain e/ADU
     key_gain = "GAIN"
-    
     
     alpha_list, P_list, Perr_list = [], [], []
     for x in args.inp:
         # Read input files
         df_in = pd.read_csv(x, sep=" ")
         N_fits = len(df_in)
+        # 1 set = 4 angels x 2 o/e
         N_fits_per_set = 8
+        # The number of sets
         N_set = int(N_fits/N_fits_per_set)
          
         for idx_set in range(N_set):
             print("")
             print(f"Start analysis of {idx_set+1}/{N_set}-th set")
-            # List to save results 
+            # List to save results of a set
             df_res_list = []
             for idx_fi in range(N_fits_per_set):
                 fi = df_in.at[idx_set*N_fits_per_set+idx_fi, "fits"]
@@ -113,7 +114,7 @@ if __name__ == "__main__":
                 hdr = src.header 
 
                 # Judge Ordinary or Extra-ordinary from fitsname
-                # At least applicable 2010 XC15 data on December 2022
+                # At least applicable 2010 XC15 data in December 2022
                 # like 'wfgs2_221220_0001.2010XC15.Rc.e.cr.fits'
                 o_or_e = fi.split(".")[-3]
 
@@ -134,37 +135,36 @@ if __name__ == "__main__":
                 print(f"    Exposure time {t_exp} s")
 
                 # Save photometry info.
-                # Assume e -> o -> e -> o ...
+                # assuming e -> o -> e -> o ... .
                 if o_or_e == "e":
                     info = dict()
                 else:
                     pass
-
-
                
-                # Background subtraction ==============================================
-                # Convert to float to suppress error
+                # Background subtraction ======================================
+                # Convert to float to suppress unfavorable error
                 img = img.astype(np.float32)
-                # args.wobgsub useless?
-                if (not args.ann) and (args.wobgsub):
-                    img, bg_info = remove_background2d_Dipol2(img)
+                if args.ann:
+                    print("    !! Do not subtract background !!")
+                    # Temporally
+                    bgerr = 0
+                else:
+                    img, bg_info = remove_background2d_pol(img)
                     bgerr = np.round(bg_info["rms"], 2)
                     #print("")
                     #print(f"Subtracted Mean: {np.mean(img)}")
                     #print(f"Subtracted Median: {np.median(img)}")
                     #print(f"Estimated sky error per pixel is {bgerr} [ADU]")
                     #print("")
-                else:
-                    print("    !! Do not subtract background !!")
-                    # Temporally
-                    bgerr = 0
+
+                # Save info.
                 info["gloabalrms"] = bgerr
                 info["level_mean"] = np.mean(img)
                 info["level_median"] = np.median(img)
-                # Background subtraction ==============================================
+                # Background subtraction ======================================
 
 
-                # Source detection ====================================================
+                # Source detection ============================================
                 # 5-sigma detection
                 #dth     = 5
                 #minarea = 15
@@ -172,9 +172,10 @@ if __name__ == "__main__":
                 #N_obj   = len(objects)
                 #print(f"{N_obj} objects detected on {idx_fi+1}-th fits {fi}.")
                 
-                # Original coordinates
-                x1 = df_in.at[idx_set*N_fits_per_set+idx_fi, "x"]
-                y1 = df_in.at[idx_set*N_fits_per_set+idx_fi, "y"]
+                # Original coordinates of the target
+                x0 = df_in.at[idx_set*N_fits_per_set+idx_fi, "x"]
+                y0 = df_in.at[idx_set*N_fits_per_set+idx_fi, "y"]
+                print(f"  {fi}, {x0}, {y0}")
 
                 # # Search the most suitable objects
                 # x_base, y_base = objects["x"], objects["y"]
@@ -184,10 +185,10 @@ if __name__ == "__main__":
 
                 # # New barycenters
                 # x1, y1 = res
-                # Source detection ====================================================
+                # Source detection ============================================
 
 
-                # Do photometry =======================================================
+                # Do photometry ===============================================
                 if args.ann:
                     ann_gap = args.ann_gap
                     ann_width = args.ann_width
@@ -198,34 +199,31 @@ if __name__ == "__main__":
                 # fluxerr**2 = bgerr_per_pix**2*N_pix + Poission**2
                 #            = bgerr_per_pix**2*N_pix + (flux*gain)/gain**2
                 flux, fluxerr, eflag = sep.sum_circle(
-                    img, [x1], [y1], r=radius, err=bgerr, gain=gain,
+                    img, [x0], [y0], r=radius, err=bgerr, gain=gain,
                     bkgann=bkgann)
                 flux, fluxerr = float(flux), float(fluxerr)
-                # Do photometry =====================================================
+                # Do photometry ===============================================
 
 
                 # Plot photometry region ======================================
                 if args.photmap:
-                    out = os.path.join(
-                        photmapdir, f"{fi}_photmap.png")
-
-                    label = f"{args.obj} (x, y)=({x1}, {y1})"
+                    out = os.path.join(photmapdir, f"{fi}_photmap.png")
+                    label = f"{args.obj} (x, y)=({x0}, {y0})"
                     color = mycolor[0]
                     ls = myls[0]
 
                     # Plot src image after 5-sigma clipping 
                     sigma = 5
                     _, vmin, vmax = sigmaclip(img, sigma, sigma)
-                    ny, nx = img.shape
 
                     fig = plt.figure(figsize=(12,int(12*ny/nx)))
                     ax = fig.add_subplot(111)
                     ax.imshow(img, cmap='gray', vmin=vmin, vmax=vmax)
                     ax.scatter(
-                        x1, y1, color=color, s=radius, lw=1, 
+                        x0, y0, color=color, s=radius, lw=1, 
                         facecolor="None", alpha=1, label=label)
                     ax.add_collection(PatchCollection(
-                        [Circle((x1,y1), radius)],
+                        [Circle((x0, y0), radius)],
                         color=color, ls=ls, lw=1, facecolor="None", label=None)
                         )
                     ax.set_xlim([0, nx])
@@ -238,7 +236,7 @@ if __name__ == "__main__":
                 # Plot photometry region ======================================
 
 
-                # Noise calculation ===================================================
+                # Noise calculation ===========================================
                 # if idx_fi == 0:
                 #     ## Background noise (sum of background + readout)
                 #     med, std = np.median(img), np.std(img)
@@ -269,14 +267,18 @@ if __name__ == "__main__":
                 #     sumerr_e = adderr(std_sum, fluxerr_e_ADU)
                 #     print(f"Sum of noise (o, e) = {sumerr_o:.2f} {sumerr_e:.2f} ADU")
                 #     print(f", which should be close to fluxerr {fluxerr_o:.3f} and {fluxerr_e:.3f} ADU (returns of sep)\n")
-                # # Noise calculation ===================================================
+                # # Noise calculation =========================================
 
                 info[f"flux_{o_or_e}"]    = flux
                 info[f"fluxerr_{o_or_e}"] = fluxerr
+                print(
+                    f"    flux, fluxerr, SNR = "
+                    f"{flux:.2f}, {fluxerr:.2f}, {flux/fluxerr:.1f}"
+                    )
 
                 # Assume e (rot X)-> o (rot X)-> e (rot Y)-> o (rot Y) ...
                 if o_or_e == "e":
-                    # Rotation angle 0, 22.5 deg (22.5 deg -> 2250)
+                    # Rotation angle 0, 22.5 deg (22.5 deg -> 2250), ...
                     ang = hdr[key_ang]
                     info["angle"] = f"{int(ang*10):04d}"
                     date = hdr[key_date]
@@ -287,8 +289,7 @@ if __name__ == "__main__":
                     df_res = pd.DataFrame(info.values(), index=info.keys()).T
                     df_res_list.append(df_res)
                     print(f"Ratio e/o = {info[f'flux_e']/info['flux_o']}")
-                    assert False, df_res
-            # 1 set results (Length = 4)
+            # Concatenate results of the set (Length = 4)
             df_res = pd.concat(df_res_list, axis=0)
             df_res = df_res.reset_index()
         
@@ -298,13 +299,13 @@ if __name__ == "__main__":
             P_list.append(P)
             Perr_list.append(Perr)
 
-            # Obtain phase angle with object name
-            # Use the first time
-            ut = df_res.at[0, "utc"]
-            alpha = utc2alpha(args.obj, ut, args.loc)
-            alpha_list.append(alpha)
+            if args.mp:
+                # Obtain phase angle with object name
+                # Use the first time
+                ut = df_res.at[0, "utc"]
+                alpha = utc2alpha(args.obj, ut, args.loc)
+                alpha_list.append(alpha)
 
-        
         # Round parameters
         # Save results
         if args.out:
@@ -312,7 +313,12 @@ if __name__ == "__main__":
         else:
             out = "polres_WFGS2.txt"
         out = os.path.join(outdir, out)
-        df_all = pd.DataFrame(dict(
-            alpha=alpha_list, P=P_list, Perr=Perr_list
+        if args.mp:
+            df_all = pd.DataFrame(dict(
+                alpha=alpha_list, P=P_list, Perr=Perr_list
+                ))
+        else:
+            df_all = pd.DataFrame(dict(
+                P=P_list, Perr=Perr_list
             ))
         df_all.to_csv(out, sep=" ", index=False)
